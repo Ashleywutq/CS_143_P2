ac7cdf232ad373e795ede73c30c94a581cf1e9f0
Ashley <ashley-wu@live.com>
2019-05-30 20:36:28 -0700

    task 7

--- reddit_model.py
+++ reddit_model.py
@@ -4,6 +4,10 @@ from pyspark.sql import SQLContext
 from pyspark.sql.functions import udf,expr
 from pyspark.sql.types import ArrayType, StringType
 from pyspark.ml.feature import CountVectorizer
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+
 
 import cleantext
 
@@ -47,14 +51,51 @@ def main(context):
     #Task 6A
     cv = CountVectorizer(inputCol="cleaned_body", outputCol="features", binary = True, minDF=10.0)
     model = cv.fit(data)
-
-    result = model.transform(data)
-    result.show(truncate=False)
+    data = model.transform(data)
 
     #Task 6B
-    data = data.withColumn("positive", expr("case when labeldjt = '1' then 1 else 0 end"))
-    data = data.withColumn("negative", expr("case when labeldjt = '-1' then 1 else 0 end"))
-    print(data.limit(1).collect())
+    pos = data.withColumn("label", expr("case when labeldjt = '1' then 1 else 0 end"))
+    neg = data.withColumn("label", expr("case when labeldjt = '-1' then 1 else 0 end"))
+
+    #Task 7
+    # Initialize two logistic regression models.
+    # Replace labelCol with the column containing the label, and featuresCol with the column containing the features.
+    poslr = LogisticRegression(labelCol='label', featuresCol="features", maxIter=10)
+    neglr = LogisticRegression(labelCol='label', featuresCol="features", maxIter=10)
+    # This is a binary classifier so we need an evaluator that knows how to deal with binary classifiers.
+    posEvaluator = BinaryClassificationEvaluator()
+    negEvaluator = BinaryClassificationEvaluator()
+    # There are a few parameters associated with logistic regression. We do not know what they are a priori.
+    # We do a grid search to find the best parameters. We can replace [1.0] with a list of values to try.
+    # We will assume the parameter is 1.0. Grid search takes forever.
+    posParamGrid = ParamGridBuilder().addGrid(poslr.regParam, [1.0]).build()
+    negParamGrid = ParamGridBuilder().addGrid(neglr.regParam, [1.0]).build()
+    # We initialize a 5 fold cross-validation pipeline.
+    posCrossval = CrossValidator(
+        estimator=poslr,
+        evaluator=posEvaluator,
+        estimatorParamMaps=posParamGrid,
+        numFolds=5)
+    negCrossval = CrossValidator(
+        estimator=neglr,
+        evaluator=negEvaluator,
+        estimatorParamMaps=negParamGrid,
+        numFolds=5)
+    # Although crossvalidation creates its own train/test sets for
+    # tuning, we still need a labeled test set, because it is not
+    # accessible from the crossvalidator (argh!)
+    # Split the data 50/50
+    posTrain, posTest = pos.randomSplit([0.5, 0.5])
+    negTrain, negTest = neg.randomSplit([0.5, 0.5])
+    # Train the models
+    print("Training positive classifier...")
+    posModel = posCrossval.fit(posTrain)
+    print("Training negative classifier...")
+    negModel = negCrossval.fit(negTrain)
+    # Once we train the models, we don't want to do it again. We can save the models and load them again later.
+    posModel.save("project2/pos.model")
+    negModel.save("project2/neg.model")
+
 
 
 
