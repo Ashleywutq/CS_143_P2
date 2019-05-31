1fe79f7436efb5f3c4c8e90eb557a344f7629a87
Ashley <ashley-wu@live.com>
2019-05-31 11:17:39 -0700

    task 8.0

--- reddit_model.py
+++ reddit_model.py
@@ -17,6 +17,9 @@ def convert(text):
         final += text[i].split()
     return final  # list
 
+def stripthree(text):
+    return text[3:]
+
 def main(context):
     """Main function takes a Spark SQL context."""
     # YOUR CODE HERE
@@ -35,68 +38,73 @@ def main(context):
     submissions = context.read.parquet("submissions.parquet")
     label = context.read.parquet("label.parquet")
     
-    # Task 2 functional dependencies join two table
-    data = label.join(comments, label.Input_id == comments.id,'inner').select(label.Input_id,comments.body,label.labeldjt)
+    # # Task 2 functional dependencies join two table
+    # data = label.join(comments, label.Input_id == comments.id,'inner').select(label.Input_id,comments.body,label.labeldjt)
  
-    #Task 4 
-    sanitize = udf(cleantext.sanitize, ArrayType(StringType()))
-    # context.udf.register("sanitize", sanitize_udf)
-    # context.registerDataFrameAsTable(data, "data")
-    data = data.withColumn('cleaned_body', sanitize(data.body))
+    # #Task 4 
+    # sanitize = udf(cleantext.sanitize, ArrayType(StringType()))
+    # # context.udf.register("sanitize", sanitize_udf)
+    # # context.registerDataFrameAsTable(data, "data")
+    # data = data.withColumn('cleaned_body', sanitize(data.body))
 
-    #Task 5
-    convert_udf = udf(convert,ArrayType(StringType()))
-    data = data.withColumn('cleaned_body', convert_udf(data.cleaned_body))
+    # #Task 5
+    # convert_udf = udf(convert,ArrayType(StringType()))
+    # data = data.withColumn('cleaned_body', convert_udf(data.cleaned_body))
 
-    #Task 6A
-    cv = CountVectorizer(inputCol="cleaned_body", outputCol="features", binary = True, minDF=10.0)
-    model = cv.fit(data)
-    data = model.transform(data)
+    # #Task 6A
+    # cv = CountVectorizer(inputCol="cleaned_body", outputCol="features", binary = True, minDF=10.0)
+    # model = cv.fit(data)
+    # data = model.transform(data)
 
-    #Task 6B
-    pos = data.withColumn("label", expr("case when labeldjt = '1' then 1 else 0 end"))
-    neg = data.withColumn("label", expr("case when labeldjt = '-1' then 1 else 0 end"))
+    # #Task 6B
+    # pos = data.withColumn("label", expr("case when labeldjt = '1' then 1 else 0 end"))
+    # neg = data.withColumn("label", expr("case when labeldjt = '-1' then 1 else 0 end"))
 
-    #Task 7
-    # Initialize two logistic regression models.
-    # Replace labelCol with the column containing the label, and featuresCol with the column containing the features.
-    poslr = LogisticRegression(labelCol='label', featuresCol="features", maxIter=10)
-    neglr = LogisticRegression(labelCol='label', featuresCol="features", maxIter=10)
-    # This is a binary classifier so we need an evaluator that knows how to deal with binary classifiers.
-    posEvaluator = BinaryClassificationEvaluator()
-    negEvaluator = BinaryClassificationEvaluator()
-    # There are a few parameters associated with logistic regression. We do not know what they are a priori.
-    # We do a grid search to find the best parameters. We can replace [1.0] with a list of values to try.
-    # We will assume the parameter is 1.0. Grid search takes forever.
-    posParamGrid = ParamGridBuilder().addGrid(poslr.regParam, [1.0]).build()
-    negParamGrid = ParamGridBuilder().addGrid(neglr.regParam, [1.0]).build()
-    # We initialize a 5 fold cross-validation pipeline.
-    posCrossval = CrossValidator(
-        estimator=poslr,
-        evaluator=posEvaluator,
-        estimatorParamMaps=posParamGrid,
-        numFolds=5)
-    negCrossval = CrossValidator(
-        estimator=neglr,
-        evaluator=negEvaluator,
-        estimatorParamMaps=negParamGrid,
-        numFolds=5)
-    # Although crossvalidation creates its own train/test sets for
-    # tuning, we still need a labeled test set, because it is not
-    # accessible from the crossvalidator (argh!)
-    # Split the data 50/50
-    posTrain, posTest = pos.randomSplit([0.5, 0.5])
-    negTrain, negTest = neg.randomSplit([0.5, 0.5])
-    # Train the models
-    print("Training positive classifier...")
-    posModel = posCrossval.fit(posTrain)
-    print("Training negative classifier...")
-    negModel = negCrossval.fit(negTrain)
-    # Once we train the models, we don't want to do it again. We can save the models and load them again later.
-    posModel.save("project2/pos.model")
-    negModel.save("project2/neg.model")
+    # #Task 7
+    # # Initialize two logistic regression models.
+    # # Replace labelCol with the column containing the label, and featuresCol with the column containing the features.
+    # poslr = LogisticRegression(labelCol='label', featuresCol="features", maxIter=10)
+    # neglr = LogisticRegression(labelCol='label', featuresCol="features", maxIter=10)
+    # # This is a binary classifier so we need an evaluator that knows how to deal with binary classifiers.
+    # posEvaluator = BinaryClassificationEvaluator()
+    # negEvaluator = BinaryClassificationEvaluator()
+    # # There are a few parameters associated with logistic regression. We do not know what they are a priori.
+    # # We do a grid search to find the best parameters. We can replace [1.0] with a list of values to try.
+    # # We will assume the parameter is 1.0. Grid search takes forever.
+    # posParamGrid = ParamGridBuilder().addGrid(poslr.regParam, [1.0]).build()
+    # negParamGrid = ParamGridBuilder().addGrid(neglr.regParam, [1.0]).build()
+    # # We initialize a 5 fold cross-validation pipeline.
+    # posCrossval = CrossValidator(
+    #     estimator=poslr,
+    #     evaluator=posEvaluator,
+    #     estimatorParamMaps=posParamGrid,
+    #     numFolds=5)
+    # negCrossval = CrossValidator(
+    #     estimator=neglr,
+    #     evaluator=negEvaluator,
+    #     estimatorParamMaps=negParamGrid,
+    #     numFolds=5)
+    # # Although crossvalidation creates its own train/test sets for
+    # # tuning, we still need a labeled test set, because it is not
+    # # accessible from the crossvalidator (argh!)
+    # # Split the data 50/50
+    # posTrain, posTest = pos.randomSplit([0.5, 0.5])
+    # negTrain, negTest = neg.randomSplit([0.5, 0.5])
+    # # Train the models
+    # print("Training positive classifier...")
+    # posModel = posCrossval.fit(posTrain)
+    # print("Training negative classifier...")
+    # negModel = negCrossval.fit(negTrain)
+    # # Once we train the models, we don't want to do it again. We can save the models and load them again later.
+    # posModel.save("project2/pos.model")
+    # negModel.save("project2/neg.model")
 
-
+    #task 8
+    stripthree_udf = udf(stripthree, StringType())
+    comments_truc = comments.select(comments.created_utc, comments.link_id.alias('link_id'), comments.author_flair_text)
+    submissions_truc = submissions.select(submissions.id,  submissions.title)
+    # data2 = comments_truc.join(submissions_truc, comments_truc.link_id == submissions_truc.id,'inner')
+    print(comments_truc.limit(1).collect())
 
 
 if __name__ == "__main__":
